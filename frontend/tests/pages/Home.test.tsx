0171a8fc495ee75e8e9f0066966cec459c25534c
Ana Lins <analinsdev@gmail.com>
2026-04-13 22:13:33 -0300

    fix test comand on ci.yml

--- frontend/tests/pages/Home.test.tsx
+++ frontend/tests/pages/Home.test.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect, beforeEach, vi } from "vitest";
+
+import { mockUseDashboard } from "../mocks/useDashboard.mock";
+
 import Home from "@/app/page";
 
 import { MachinesProps } from "@/components/dashboard/MachinesDashboard";
 import { MetricProps } from "@/components/dashboard/MetricDashboard";
 import { OrdersProps } from "@/components/dashboard/OrdersDashboard";
-
-import { mockUseDashboard } from "../mocks/useDashboard.mock";
 
 vi.mock("@/components/dashboard/MetricDashboard", () => ({
   default: (props: MetricProps) => (
